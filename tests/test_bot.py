9ad98c433edbfc7c391ea9e91d8c651508ad93b4
Alex Li <sclion.by@gmail.com>
2026-04-27 19:01:32 +0300

    Improve tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,5 +1,10 @@
-from bot.service import add_expense, total
+from bot.service import add_expense, total, by_category
 
 def test_total():
     add_expense("Test", 5)
     assert total() >= 5
+
+def test_category():
+    add_expense("Food", 10, "food")
+    result = by_category("food")
+    assert len(result) > 0
