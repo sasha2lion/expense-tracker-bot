25e60b20940db50ffd78a52474166c7508e0dd23
Alex Li <sclion.by@gmail.com>
2026-04-27 18:46:58 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "ВСТАВЬ_СЮДА_СВОЙ_ТОКЕН"
+TOKEN = "@HellenkaBot"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот для учета расходов 💰")
